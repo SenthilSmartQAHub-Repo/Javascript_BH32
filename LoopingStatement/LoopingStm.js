e8d6243ec002670fa56084be119041e92c8d56a9
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-02-25 08:32:58 +0530

    looping

---
+++ LoopingStatement/LoopingStm.js
@@ -0,0 +1,34 @@
+
+// 1. for loop
+// 2. while loop
+// 3. do-while loop
+
+/*
+
+ for(inititalization;condition;update-increment/decrement)
+ {
+   code to execute
+ 
+ }
+
+
+ */
+
+ // print the wecome 5 times
+
+ let n=5;
+
+ for(let i=1;i<=n;i++)
+ {
+    console.log("welcome",i)
+  
+ }
+
+//====================break==========
+
+ for(let i=1;i<=n;i++)
+ {
+    console.log("welcome",i)
+    break;
+ }
+
