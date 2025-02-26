6ce2306ed7f66b1523c3bc2796c999c2fbd0a629
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-02-26 07:21:29 +0530

    looping

--- LoopingStatement/LoopingStm.js
+++ LoopingStatement/LoopingStm.js
@@ -31,4 +31,50 @@
     console.log("welcome",i)
     break;
  }
+//===========print the 5 to 1========================
 
+for(let i=5;i>=1;i--)
+{
+   console.log(i)
+}
+//========================================================
+
+// while loop:
+/* ==========
+
+while(condition)
+{
+
+   //java stm
+
+}
+*/
+let i=1;
+while(i<=5)
+{
+   console.log(`i value is ${i}`)
+   i++;
+}
+
+
+//  3. do while.........................
+
+
+/*
+
+do {
+
+//statement
+
+}while(condition);
+
+*/
+
+let k=1;
+do{
+
+   console.log(`k value is ${k}`)
+
+  k++;
+}
+while(k<=5)
