ce1a4ef289738b6a20c1685c76d875b8a71b1e8d
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-04-04 07:33:32 +0530

    async

--- DemoPromise/DemoPromise1.js
+++ DemoPromise/DemoPromise1.js
@@ -2,7 +2,7 @@
 
 let promise=new Promise((resolve,reject)=>{
 
-     let age=30;
+     let age=31;
      if(age==30)
      {
         resolve("success")
@@ -12,5 +12,26 @@ let promise=new Promise((resolve,reject)=>{
      }
 
 })
-promise.then(result=>console.log(result)).catch(error=>console.log(error))
+promise.then(result=>console.log(result)).catch(error=>console.log(error)).finally(r1=>console.log("Promise executed"))
 
+//then()--> promise resolved
+//catch()---> Promise rejected
+//finally() -->promise resolved / Promise rejected
+
+
+
+
+let promise1=new Promise((resolved)=>
+   {
+       setTimeout(()=>{resolved("Promise 1 resolved")},10000)  
+   })
+  
+   //async function
+   async function doCall()
+   {
+       console.log(await promise1)
+
+   }
+   doCall()
+
+   //await -- wait promise will return suceess or failed
