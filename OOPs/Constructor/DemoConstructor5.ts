d07bfdf0db7b4e602b7dc2ba785dcd4b37e800cb
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-04-15 07:55:02 +0530

    constructor

--- OOPs/Constructor/DemoConstructor5.ts
+++ OOPs/Constructor/DemoConstructor5.ts
@@ -1,16 +1,21 @@
 
+
 class Student1
 {
     studentName:string
     studentRollNo:string
     IsPassed:boolean
-    constructor(a:string,b:string,c:boolean)
+    constructor(studentName:string,studentRollNo:string,IsPassed:boolean)
     {
-       this.studentName=a
-       this.studentRollNo=b
-       this.IsPassed=c
+       this.studentName=studentName
+       this.studentRollNo=studentRollNo
+       this.IsPassed=IsPassed
     }
 }
 
 const stuobj1=new Student1("john","B123",true)
+const stuobj2=new Student1("paven","B124",true)
+
 console.log(stuobj1.studentName,stuobj1.studentRollNo,stuobj1.IsPassed)
+
+console.log(stuobj2.studentName,stuobj2.studentRollNo,stuobj2.IsPassed)
