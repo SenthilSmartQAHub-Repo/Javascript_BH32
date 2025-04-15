cec3e844a8328e74dac8f9ad955de9024dbb3ae5
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-04-15 08:17:00 +0530

    constructor

---
+++ OOPs/StaticandReadOnly/Notes.md
@@ -0,0 +1,10 @@
+
+
+
+Two types of Global variable 
+
+1. static variable -  variable declarad using static keyword
+                    -  static variable is refered using classname.static variable
+
+2. Non-static variable or instance variable  ----> variable declarad without static keyword
+                                             ---> instance variable is refered using this.instance variable
---
+++ OOPs/StaticandReadOnly/StaticAndRead.ts
@@ -0,0 +1,24 @@
+
+
+class Emp{
+
+
+static company:string ="HCL" // static variable
+empId:number   //non-static variable or instance variable 
+empName:string  //non-static variable or instance variable 
+
+constructor(empId:number,empName:string)
+{
+    this.empId=empId
+    this.empName=empName
+}
+
+
+}
+console.log(Emp.company)
+
+const empobj1=new Emp(123,"Arun")
+const empobj2=new Emp(125,"Laxman")
+
+console.log(empobj1.empId,empobj1.empName)
+console.log(empobj2.empId,empobj2.empName)
