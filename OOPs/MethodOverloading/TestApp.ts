4648f8fac793884a28eb6970fadf2aada800844c
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-04-21 07:38:10 +0530

    test

--- OOPs/MethodOverloading/TestApp.ts
+++ OOPs/MethodOverloading/TestApp.ts
@@ -1,11 +1,12 @@
 
-// developing multiple same methods with different parameter
+// developing multiple methods with same method name with different parameter
 
 class TestApp
 {
     add(a:number,b:number):number
     add(a:string,b:string):string
     add(a:boolean):boolean
+    
     add(a:any,b?:any):any
     {
         return a+b
@@ -15,3 +16,8 @@ const Testappobj=new TestApp()
 console.log(Testappobj.add(10,20))
 console.log(Testappobj.add("welcome","to playwright class"))
 console.log(Testappobj.add(true))
+
+// method(any method) and no retunr value -  no parameter
+//                      2 paramter
+
+//                      3 paramter
